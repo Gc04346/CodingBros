204f73fc76745f9b84884a499c04dd56e46befca
Steven Sousa <70921007+Stevensousa67@users.noreply.github.com>
2021-09-25 15:58:41 -0400

    Website started

--- todolist/todolist/tasks/views.py
+++ todolist/todolist/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import MyTasks
+
+
+def homepage(request):
+    context = {
+        'tasks': MyTasks.objects.all(),
+    }
+    return render(request, 'index.html', context=context)
