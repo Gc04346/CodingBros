c592f093e051359c0618c08f82ff3421bbd34af2
Gc04346 <gc04346@gmail.com>
2021-09-25 16:10:47 -0300

    tasks model admin

--- todolist/todolist/tasks/models.py
+++ todolist/todolist/tasks/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 class MyTasks(models.Model):
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
     class PriorityChoices(models.TextChoices):
         LOW = 'LO', 'Low'
         NORMAL = 'NO', 'Normal'
@@ -8,9 +14,12 @@ class MyTasks(models.Model):
 
     name = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     time = models.TimeField(null=True, blank=True)
     location = models.CharField(max_length=150)
     done = models.BooleanField()
-    priority = models.CharField(max_length=2, choices=PriorityChoices.CHOICES, default=PriorityChoices.NORMAL)
+    priority = models.CharField(max_length=2, choices=PriorityChoices.choices, default=PriorityChoices.NORMAL)
+    notification_due_date = models.DateTimeField(null=True, blank=True, verbose_name='User will be notified at')
 
+    def __str__(self):
+        return f'{self.name}'
